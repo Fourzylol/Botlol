83ecbb1a3f10161953eaade71c97dd28943d7823
I`am Ra <78637964+rayyreall@users.noreply.github.com>
2021-09-29 22:00:48 +0700

    pindah pak

--- index.ts
+++ index.ts
@@ -1,14 +1,1 @@
-import { Connected  } from "./lib/routers/connect/koneksi";
-import { config} from "dotenv";
-
-config({ path: __dirname + "\\.env"})
-
-class Start extends Connected {
-	constructor(){
-		super()
-	}
-	protected async Conect () {
-		await this.Connect()
-	}
-}
-new Start().Connect()
+BASE PINDAH
