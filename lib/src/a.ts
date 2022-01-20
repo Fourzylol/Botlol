1149306b1fed964a5d25c2983b8433531a728007
I`am Ra <78637964+rayyreall@users.noreply.github.com>
2022-01-20 18:19:39 +0800

    Fix Bug & add Sticker

--- lib/src/a.ts
+++ lib/src/a.ts
@@ -1,3 +1,3 @@
 export var tester = globalThis.ev.on("test-global", (client, message) => {
 	console.log("horeee work")
-}, { event: ["hay"], tag: "test global", command: /(asulu)/i, isOwner: true})
+}, { event: ["hay"], tag: "test global", command: ["hay", "syng", /(asulu)/i], isOwner: true})
