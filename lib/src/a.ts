7d05180806677d1089407acbf87e3018271db20b
I`am Ra <78637964+rayyreall@users.noreply.github.com>
2022-01-26 01:30:14 +0700

    hapus sampah

--- lib/src/a.ts
+++
@@ -1,3 +0,0 @@
-export var tester = globalThis.ev.on("test-global", (client, message) => {
-	console.log("horeee work")
-}, { event: ["hay"], tag: "test global", command: ["hay", "syng", /(asulu)/i], isOwner: true})
